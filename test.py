005cc4f2e0bf3b510b74c7f73b07beb2c29de520
Brian Liu <b_liu72@yahoo.com>
2017-04-08 20:09:02 -0400

    added endpoints

--- test.py
+++ test.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "This is the homepage"
+
+@app.route("/<path:path>")
+def rest(path):
+    return "You have hit the " + path + " endpoint."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
